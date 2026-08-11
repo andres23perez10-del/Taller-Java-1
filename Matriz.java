bcd9c7b67888397bff690792ebcc8aef529680b0
andres23perez10-del <andres23perez10@gmail.com>
2026-08-10 21:16:14 -0500

    Add files via upload

---
+++ Matriz.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.taller_1;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class Matriz {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la altura de la pirámide (n): ");
+        int n = scanner.nextInt();
+
+        System.out.print("¿Desea centrar las pirámides? (1: Sí / 0: No): ");
+        int centrar = scanner.nextInt();
+
+        System.out.println("\n--- PIRÁMIDE NORMAL ---");
+        for (int i = 1; i <= n; i++) {
+            if (centrar == 1) {
+                for (int espacios = 0; espacios < n - i; espacios++) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n--- PIRÁMIDE INVERTIDA ---");
+        for (int i = n; i >= 1; i--) {
+            if (centrar == 1) {
+                for (int espacios = 0; espacios < n - i; espacios++) {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
+
